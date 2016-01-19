4b96e880439263c8ad2d58d6065c5a7f1b9c6414
Josh Morel <morel.josh@hotmail.com>
2016-01-19 18:26:36 -0500

    Added median age

--- code/rawcode/exploratory.R
+++ code/rawcode/exploratory.R
@@ -1,14 +1,11 @@
 library(data.table)
 library(ggplot2)
 library(ggvis)
+library(MASS)
 
-setwd("~/../killed_by_police/code/rawcode")
-thecounted_and_crime = fread("../../data/thecounted_and_crime.csv",showProgress=FALSE)
-thecounted_and_crime_labels = fread("../../data/killed_by_police_code_book.csv",showProgress=FALSE)
+thecounted_and_crime <- fread("data/thecounted_and_crime.csv",showProgress=FALSE)
+thecounted_and_crime_labels <- fread("data/killed_by_police_code_book.csv",showProgress=FALSE)
 
-plot_labels = setNames(thecounted_and_crime_labels$variable,thecounted_and_crime_labels$label)
-
-#thecounted_and_crime = subset(thecounted_and_crime,state!="DC")
 
 #Rate of People Killed by Police in US
 ggplot(thecounted_and_crime,aes(x=killedbypolice2015_per100k,y=reorder(state,killedbypolice2015_per100k))) +
@@ -22,18 +19,39 @@ ggplot(thecounted_and_crime,aes(x=killedbypolice2015_per100k,y=reorder(state,kil
         theme(axis.text.y = element_text(hjust = grid::unit(c(-1, 0), "points")))
 
 
-#Rate of People Killed by Police in US
+# Basic histogram of Rate Killed by Police, it looks approximately log normal
+killedrate_range <- diff(range(thecounted_and_crime$killedbypolice2015_per100k))
+ggplot(thecounted_and_crime,aes(x=killedbypolice2015_per100k)) + 
+        geom_density(binwidth=killedrate_range/11,fill="white") + 
+        stat_function(fun=dlnorm,args=list(meanlog=mean(log(thecounted_and_crime$killedbypolice2015_per100k)),
+                                           sd=sd(log(thecounted_and_crime$killedbypolice2015_per100k))),
+                      color="red")
 
-ggplot(thecounted_and_crime,aes(x=reorder(state,killedbypolice2015_per100k),y=killedbypolice2015_per100k)) +
-        geom_segment(aes(xend = state), size=1,yend = 0, colour ="#D2D2F0",guide=FALSE) +
-        geom_point(aes(size=killedbypolice2015)) +
-        scale_colour_brewer(palette ="Set1", guide = FALSE) +
-        theme_bw() +
-        theme(panel.grid.major.y = element_blank()) +
-        coord_flip() +
-        ggtitle("Rate of People Killed by Police in the U.S. in 2015 by State") +
-        labs(x = "State", y = "Killed per 100k Population", size="Number Killed\nby Police")
+# Distribution of outcome variable appears approximately log-normal 
+killedratelog_range <- diff(range(log(thecounted_and_crime$killedbypolice2015_per100k)))
 
+ggplot(thecounted_and_crime,aes(x=log(killedbypolice2015_per100k))) + 
+        geom_density(binwidth=killedratelog_range/11,fill="white") + 
+        stat_function(fun=dnorm,args=list(mean=mean(log(thecounted_and_crime$killedbypolice2015_per100k)),
+                                          sd=sd(log(thecounted_and_crime$killedbypolice2015_per100k))),
+                      color="red")
+
+
+
+# Plotting x vs y
+
+#Violent Crime vs Killed
+ggplot(thecounted_and_crime,aes(x=log(violent_crime2014_per100k),y=log(killedbypolice2015_per100k))) + 
+        geom_point() + geom_smooth(method="lm")
+
+#Population Black vs Killed
+ggplot(thecounted_and_crime,aes(x=median_age2014,y=log(killedbypolice2015_per100k))) + 
+        geom_point() + geom_smooth(method="lm")
+
+
+plot_labels = setNames(thecounted_and_crime_labels$variable,thecounted_and_crime_labels$label)
+
+#thecounted_and_crime = subset(thecounted_and_crime,state!="DC")
 
 #Against population
 cor(thecounted_and_crime[,.(population2014,population_black2014,killedbypolice2015,violent_crime2014,murder_nonnegligent_manslaughter2014,police_officers2014)])
@@ -66,16 +84,66 @@ thecounted_and_crime_no_dc = thecounted_and_crime[state != "DC"]
 #Explanatory = Violent Crime, response = Killed by Police
 #Since we are dealing with counts data a Generalized Linear Model of Poisson family is suited to model
 #The relationship. However, as the response variable - killedbypolice2015 - does not follow the Mean - Variance relationship o fpoisson, we will use quasi-poisson instead
-glm.killed <- glm(killedbypolice2015 ~ log(police_officers2014) + log(violent_crime2014) + log(population_black2014),offset=log(population2014),family="quasipoisson",data=thecounted_and_crime)
 
+glm.killed <- glm(killedbypolice2015 ~ log(police_officers2014) + log(violent_crime2014) + 
+                          log(population_black2014) + log(population_hispanic2014) +
+                          log(median_age2014) + log(population_male2014),
+                  offset=log(population2014),family="quasipoisson",data=thecounted_and_crime)
+
+summary(glm.killed)
+library(MASS)
+glm.killed <- stepAIC(glm.killed,direction="both")
+summary(glm.killed)
+
+#Using poisson
+
+glm.mod <- glm(killedbypolice2015 ~ log(violent_crime2014),offset=log(population2014), family=poisson,data = thecounted_and_crime)
+
+library(AER)
+#testing for dispersion
+
+glm.mod <- glm(killedbypolice2015 ~ log(violent_crime2014),offset=log(population2014), family=poisson,data = thecounted_and_crime)
+dispersiontest(glm.mod)
+
+#Using linear model with log transformed
+
+lm.killed <- lm(log(killedbypolice2015_per100k) ~ log(police_officers2014_per100k) + log(violent_crime2014_per100k) +
+                        population_black2014_percent + population_male2014_percent + population_hispanic2014_percent + 
+                        median_age2014,data=thecounted_and_crime)
+
+summary(lm.killed)
+library(MASS)
+lm.killed <- stepAIC(lm.killed,direction="both")
+summary(lm.killed)
+
+
+#Using negative binomial
+
+nb.killed <- glm.nb(killedbypolice2015 ~ median_age2014 + log(violent_crime2014) + 
+                            log(police_officers2014) + log(population_black2014) + 
+                            log(population_hispanic2014) + log(population_male2014) + offset(log(population2014)),
+                    data=thecounted_and_crime)
+
+summary(nb.killed)
+glm.killed <- stepAIC(nb.killed,direction="both")
+summary(nb.killed)
+
+estimates <- predict(nb.killed, type="response", se.fit=T)$fit
+estimates_se <- predict(nb.killed, type="response", se.fit=T)$se.fit
+estimates_lower <-  estimates - 1.96*estimates_se
+estimates_upper <-  estimates + 1.96*estimates_se
+
+estimates_df <- as.data.frame(cbind(killedbypolice2015= thecounted_and_crime$killedbypolice2015,population2014=thecounted_and_crime$population2014,estimates,estimates_se,estimates_lower,estimates_upper))
+
+ggplot(data=estimates_df,aes(x=log(population2014),y=(killedbypolice2015)) + geom_point()
 
 #Examining residuals vs estimated values
 
 killed_estimates <- data.frame(estimated = glm.killed$fitted.values,residuals = resid(glm.killed),
-                            lower = lower,
-                            upper = upper,
-                            actual = thecounted_and_crime$killedbypolice2015,
-                            state = thecounted_and_crime$state
+                               lower = lower,
+                               upper = upper,
+                               actual = thecounted_and_crime$killedbypolice2015,
+                               state = thecounted_and_crime$state
 )
 killed_estimates$actual_above_estimate = ifelse(killed_estimates$actual > killed_estimates$estimated,"Actual > Estimated","Estimated > Actual")
 killed_estimates$actual_above_estimate = factor(killed_estimates$actual_above_estimate)
@@ -147,7 +215,7 @@ make_plot <- function(xvar,yvar) {
                 layer_points(fill:="darkred") %>%
                 layer_text(text:=~state,opacity:=1) %>%
                 layer_model_predictions(model = input_radiobuttons(label = "Choose model for fitted line:", choices = c("loess","lm")), se = TRUE)
-
+        
 }
 
 
--- code/rawcode/processing.R
+++ code/rawcode/processing.R
@@ -9,13 +9,13 @@ library(xlsx)
 #2) Population - Data from U.S. Census Beureau
 #3) Crime & Police Stats - Data from FBI
 
-#1) 
+#1) The Counted
 #Loading data downloaded from The Counted - http://www.theguardian.com/us-news/ng-interactive/2015/jun/01/about-the-counted
-thecounted = fread("../../data/the-counted.csv",showProgress=FALSE)
+thecounted = fread("data/the-counted.csv",showProgress=FALSE)
 
 #Counting killings by state, need to calculate # killed to sort in plot. 
 #Equivalent in dplyr of: group_by(thecounted,state) %>% summarize(killed=n()) %>% arrange(-killed)
-thecounted_by_state = thecounted[,.(killed=.N),by = .(state)][order(-killed)]
+thecounted_by_state <- thecounted[,.(killed=.N),by = .(state)][order(-killed)]
 
 ggplot(thecounted_by_state[1:25,],aes(x=reorder(state,killed),y=killed)) + 
         geom_bar(stat='identity') + 
@@ -24,45 +24,63 @@ ggplot(thecounted_by_state[1:25,],aes(x=reorder(state,killed),y=killed)) +
         ggtitle("Number of People Killed by Police by State\n2015")
 
 
-#2)
+#2) Population
+#a) Overall population by state
 #As state/race populations vary significantly, counts are not useful for comparison so so demographic data is required
 
-state_pop = fread("http://www.census.gov/popest/data/state/asrh/2014/files/SCPRC-EST2014-18+POP-RES.csv",showProgress=FALSE)
-state_codes = fread("https://raw.githubusercontent.com/kitjosh1050/dataquest_p04_police_killings/master/state_codes.csv",showProgress = FALSE)
+state_pop <- fread("http://www.census.gov/popest/data/state/asrh/2014/files/SCPRC-EST2014-18+POP-RES.csv",showProgress=FALSE)
+state_codes <- fread("https://raw.githubusercontent.com/kitjosh1050/dataquest_p04_police_killings/master/state_codes.csv",showProgress = FALSE)
 
 #Exclude national and Puerto Rico, equivalent in ddplyr of 
 #Equivalent in dplyr of: filter(state_pop,STATE %in% c(0,72)) %>% select(NAME,POPESTIMATE2014) %>% rename(name=NAME)
-state_pop = state_pop[!(STATE %in% c(0,72)),.(name=NAME,POPESTIMATE2014,STATE)]
+state_pop <- state_pop[!(STATE %in% c(0,72)),.(name=NAME,POPESTIMATE2014,STATE)]
 
-#This is an inner join in data.table (state_pop looking up values in state_codes on set key and only returning matches)
-#Equivalent in base R of: merge(state_pop,state_codes,by.x="name",by.y="name")
-setkey(state_codes,name)
-state_pop = state_codes[state_pop]
+state_pop <- merge(state_codes,state_pop,by.x="name",by.y="name")
 
-race_pop = fread("http://www.census.gov/popest/data/state/asrh/2014/files/SC-EST2014-ALLDATA6.csv",showProgress = FALSE)
-race_pop = race_pop[ORIGIN == 0 & SEX==0]
-race_pop[,POPESTIMATE2014_black := ifelse(RACE==2,POPESTIMATE2014,0)]
-race_pop = race_pop[,.(population2014=sum(POPESTIMATE2014),population_black2014=sum(POPESTIMATE2014_black)),by=.(STATE)]
-setkey(race_pop,STATE)
-setkey(state_pop,STATE)
+#b) Median Age
+#Obtain median age by calculating cumulative sum by increasing age within each state
+#then taking the minimum age where the cumulative sum as percent of total population is greater than .50
 
-state_pop = state_pop[race_pop
-                      ][,.(name,state,population2014=POPESTIMATE2014,population_black2014)]
+race_pop <- fread("http://www.census.gov/popest/data/state/asrh/2014/files/SC-EST2014-ALLDATA6.csv",showProgress = FALSE)
 
-#This is a left outer join in data.table
-#Equivalent in base R of: merge(state_pop,thecounted_by_state,by.x="state",by.y="state",all.x =TRUE)
-#Also used here is chaining, where code in each pair of square brackets is executed in order, left to right, similar to %>% in dplyr
-setkey(state_pop,state)
-setkey(thecounted_by_state,state)
-thecounted_by_state = thecounted_by_state[state_pop,nomatch=NA][is.na(killed),killed:=0
-                                                                ][,killed_per100k:=killed/population2014*100000
-                                                                  ][order(-killed_per100k)]
+
+state_by_age <- race_pop[ORIGIN == 0 & SEX==0 & !(STATE %in% c(0,72))
+                         ][,.(population2014 = sum(POPESTIMATE2014)),by = .(STATE,AGE)
+                           ][,.(AGE,totalpop = sum(population2014),
+                                cumulativesum = cumsum(population2014),
+                                pctoftotal=cumsum(population2014)/sum(population2014)),
+                             by=.(STATE)
+                             ][pctoftotal>=.5,.(median_age=min(AGE)),by=.(STATE)]
+
+
+state_by_age <- merge(state_pop,state_by_age,by.x="STATE",by.y="STATE")
+
+#c) Percent Male, Black & Hispanic
+
+state_by_race_gender <- race_pop[,.(population_male2014 = sum(ifelse(ORIGIN==0 & SEX==1,POPESTIMATE2014,0)),
+                                    population_black2014 = sum(ifelse(ORIGIN==0 & SEX==0 & RACE==2,POPESTIMATE2014,0)),
+                                    population_hispanic2014 = sum(ifelse(ORIGIN==2 & SEX==0,POPESTIMATE2014,0))
+                                 ),by=.(STATE)]
+
+state_pop_full <- merge(state_by_age,state_by_race_gender,by.x="STATE",by.y="STATE")
+
+
+#Combine 1 & 2
+#Used here is chaining, where code in each pair of square brackets is executed in order, 
+#left to right, similar to %>% in dplyr
+
+thecounted_pop <- merge(state_pop_full,thecounted_by_state,by.x="state",by.y="state",all.x=TRUE)
+
+thecounted_pop[,c("killed","killed_per100k") := list(ifelse(is.na(killed),0,killed),
+                                                          ifelse(is.na(killed),0,killed)/POPESTIMATE2014*100000)
+                    ]
 
 
 
 #Killed by state by population Cleveland Dot Plot
-
-ggplot(thecounted_by_state[1:25,],aes(x=reorder(state,killed_per100k),y=killed_per100k,size=killed)) + 
+#set order first using data.table function
+setorder(thecounted_pop,-killed_per100k)
+ggplot(thecounted_pop[1:25,],aes(x=reorder(state,killed_per100k),y=killed_per100k,size=killed)) + 
         coord_flip() +
         labs(x = " State (Top 25)", y = "Killed per 100k Population") + 
         geom_segment(aes(xend = state, size = .01), yend = 0, colour ="#D2D2F0") +
@@ -72,58 +90,86 @@ ggplot(thecounted_by_state[1:25,],aes(x=reorder(state,killed_per100k),y=killed_p
 
 #3)
 #From FBI - Crime in the U.S. in 2014 from FBI 2014  
-violent_crime = data.table(read.xlsx("../../data/table_5_crime_in_the_united_states_by_state_2014.xls",sheetIndex=1,startRow=4,header=TRUE,stringsAsFactors=FALSE))
+violent_crime <- data.table(read.xlsx("data/table_5_crime_in_the_united_states_by_state_2014.xls",sheetIndex=1,startRow=4,header=TRUE,stringsAsFactors=FALSE))
 
 #In data.table, to refer to columns by number in j section, with=FALSE argument is required, see data.table FAQ for rationale
-numeric_cols = colnames(violent_crime[,4:14,with=FALSE])
+numeric_cols <- colnames(violent_crime[,4:14,with=FALSE])
 
 #.SD is Sub Data.table, with which a function can be applied when the applicable columns are specified with .SDcols argument
 violent_crime[,(numeric_cols):=lapply(.SD,function(x) as.numeric(ifelse(x==" ",NA,x))),.SDcols=numeric_cols]
 
 #A "fill down" method can be applied by grouping on an aggregate function
 violent_crime[,State := State[1], by = cumsum(!is.na(State))]
-violent_crime = violent_crime[grepl("Total",Area) & State != "PUERTO RICO"]
+violent_crime <- violent_crime[grepl("Total",Area) & State != "PUERTO RICO"]
 
 violent_crime[,State := trimws(tolower(gsub("[0-9,]","",violent_crime[,State])))]
 
-setkey(violent_crime,State)
-thecounted_by_state[,name := tolower(name)]
-setkey(thecounted_by_state,name)
-
-crime_counted = thecounted_by_state[violent_crime]
+thecounted_pop[,name := tolower(name)]
+thecounted_pop_crime <- merge(thecounted_pop,violent_crime,by.x="name",by.y="State")
 
 #from same FBI Crime in U.S. - Police Officers 
 #Note: first two Male/Female columns are police officers, second two are civilian employees
-police = data.table(read.xlsx("../../data/Table_77_Full_time_Law_Enforcement_Employess_by_State_2014.xls",sheetIndex=1,startRow=5,header=TRUE,stringsAsFactors=FALSE))
+police <- data.table(read.xlsx("data/Table_77_Full_time_Law_Enforcement_Employess_by_State_2014.xls",sheetIndex=1,startRow=5,header=TRUE,stringsAsFactors=FALSE))
 police[,police_officers:=Male+Female]
 police[,State := trimws(tolower(gsub("\n","",NA.)))]
 
 
 #West Viriginia is missing in 2014 because did not submit data to FBI in time, use 2013 instead
-police2013 = data.table(read.xlsx("../../data/table_77_full_time_law_enforcement_employess_by_state_2013.xls",sheetIndex=1,startRow=5,header=TRUE,stringsAsFactors=FALSE))
+police2013 <- data.table(read.xlsx("data/table_77_full_time_law_enforcement_employess_by_state_2013.xls",sheetIndex=1,startRow=5,header=TRUE,stringsAsFactors=FALSE))
 police2013[,police_officers:=Male+Female]
 police2013[,State := trimws(tolower(gsub("\n","",NA.)))]
-police2013 = police2013[State=="west virginia"]
+police2013 <- police2013[State=="west virginia"]
 
-police =rbindlist(list(police,police2013))
-police = police[State != " NA"]
+police <- rbindlist(list(police,police2013))
+police <- police[State != " NA"]
 
-setkey(police,State)
+thecounted_pop_crime_police <- merge(thecounted_pop_crime,police,by.x="name",by.y="State")
 
-combined_state_stats = police[crime_counted][,.(state,State,population2014,population_black2014,killed,Violent.crime1,Murder.and..nonnegligent..manslaughter,police_officers)]
 
-setnames(combined_state_stats,c("State","killed","Violent.crime1","Murder.and..nonnegligent..manslaughter","police_officers"),c("state_name","killedbypolice2015","violent_crime2014","murder_nonnegligent_manslaughter2014","police_officers2014"))
+## Drop, rename and reorder columns for better organized data set 
+thecounted_pop_crime_police <- thecounted_pop_crime_police[,.(state,
+                                                              name,
+                                                              median_age,
+                                                              POPESTIMATE2014,
+                                                              population_male2014,
+                                                              population_black2014,
+                                                              population_hispanic2014,
+                                                              killed,
+                                                              Violent.crime1,
+                                                              Murder.and..nonnegligent..manslaughter,
+                                                              police_officers)
+                                                           ]
 
-combined_state_stats[,c("population_black2014_percent",
+setnames(thecounted_pop_crime_police,
+         c("name",
+           "median_age",
+           "POPESTIMATE2014",
+           "killed",
+           "Violent.crime1",
+           "Murder.and..nonnegligent..manslaughter",
+           "police_officers")
+         ,c("state_name",
+            "median_age2014",
+            "population2014",
+            "killedbypolice2015",
+            "violent_crime2014",
+            "murder_nonnegligent_manslaughter2014",
+            "police_officers2014"))
+
+thecounted_pop_crime_police[,c("population_male2014_percent",
+                        "population_black2014_percent",
+                        "population_hispanic2014_percent",
                         "killedbypolice2015_per100k",
                         "violent_crime2014_per100k",
                         "murder_nonnegligent_manslaughter2014_per100k",
                         "police_officers2014_per100k"):=
-                             list(population_black2014/population2014,
+                             list(population_male2014/population2014,
+                                  population_black2014/population2014,
+                                  population_hispanic2014/population2014,
                                   killedbypolice2015/population2014*100000,
                                   violent_crime2014/population2014*100000,
                                   murder_nonnegligent_manslaughter2014/population2014*100000,
                                   police_officers2014/population2014*100000)]
 
 
-write.csv(combined_state_stats,"../../data/thecounted_and_crime.csv",row.names = FALSE)
+write.csv(thecounted_pop_crime_police,"data/thecounted_and_crime.csv",row.names = FALSE)
